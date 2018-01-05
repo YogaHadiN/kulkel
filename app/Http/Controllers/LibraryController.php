aafe1fe6d4e8afbb490b69b36b43056e27efe958
YogaHadiN <yoga_email@yahoo.com>
2018-01-05 15:09:23 +0700

    perbaiki directory

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -144,7 +144,7 @@ class LibraryController extends Controller
 	public function import(){
 		$file      = Input::file('file');
 		$file_name = $file->getClientOriginalName();
-		$file->move(url('/'), $file_name);
+		$file->move('/var/www/kulkel/public');
 		$results   = Excel::load('files/' . $file_name, function($reader){
 			$reader->all();
 		})->get();
