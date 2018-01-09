f01c1373f1bbe905bcab7287a503a6876aa71eb1
YogaHadiN <yoga_email@yahoo.com>
2018-01-09 07:54:33 +0700

    perintah test untuk panggil residen

---
+++ app/Console/Commands/test.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\User;
+
+class test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+		$users = User::where('role_id', '1')->get();
+		$data = [];
+		foreach ($users as $user) {
+			$data[] = $user->nama;
+		}
+
+		return dd( $data );
+    }
+}
