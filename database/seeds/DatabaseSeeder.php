d0c56973b6cb03a90a1d9116d992b4e433d776bf
YogaHadiN <yoga_email@yahoo.com>
2018-02-06 10:32:29 +0700

    database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(JenisUjiansTableSeeder::class);
+         $this->call(BulanColumnJenisStasesTableSeeder::class);
     }
 }
